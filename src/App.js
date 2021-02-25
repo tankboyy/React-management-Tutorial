7bf2e4d99c30428064c6f5a521f2fc84472671a3
tankboyy <tankboya@naver.com>
2021-02-25 17:15:03 +0900

    Apply Material Ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer'
+import React from 'react';
+import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root : {
+    width : '100%',
+    margintop : theme.spacing.unit * 3,
+    overflowX : "auto"
+  },
+  table : {
+    minwidth : 1080
+  }
+})
 
 const coustomers = [
   {
@@ -29,26 +47,29 @@ const coustomers = [
   }
 ]
 
-function App() {
+class App extends React.Component {
+  render() {
+  const { classes } = this.props;
   return (
-    <div>
-    {
-      coustomers.map(c => {
-        return(
-          <Customer
-          key={c.id}
-          id={c.id}
-          image={c.image}
-          name={c.name}
-          birthday={c.birthday}
-          gender={c.gender}
-          job={c.job}
-          />
-        )
-      })
-    }
-    </div>
-  );
+    <Paper className = {classes.root}>
+      <Table className = {classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody> {coustomers.map(c => { return( <Customer key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> )})}
+        </TableBody>
+      </Table>
+      </Paper>
+  )
+}
 }
 
-export default App;
+
+export default withStyles(styles)(App);
