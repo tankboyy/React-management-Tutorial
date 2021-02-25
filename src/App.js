dad8fe5511ea159d4e6b4fc020cf76fab22a0b13
tankboyy <tankboya@naver.com>
2021-02-25 16:27:02 +0900

    create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const coustomers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '킹석중',
+  'birthday': '981112',
+  'gender': '남자',
+  'job': '대통령'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김용철',
+    'birthday': '980119',
+    'gender': '개',
+    'job': '애완용'
+  },
+  {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '이호영',
+      'birthday': '980224',
+      'gender': '남자',
+      'job': '비서'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello react
-        </p>
-      </header>
+    <div>
+    {
+      coustomers.map(c => {
+        return(
+          <Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+          />
+        )
+      })
+    }
     </div>
   );
 }
