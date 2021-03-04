88350f566c3884f5a44fc87bee6cae5b34dcb434
tankboyy <tankboya@naver.com>
2021-03-04 19:38:07 +0900

    end

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react';
 import Customer from './components/Customer';
 import CustomerAdd from './components/CustomerAdd';
-
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -11,20 +10,87 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 const styles = theme => ({
   root : {
     width : '100%',
-    margintop : theme.spacing(3),
-    overflowX : "auto"
+    minWidth: 1080
   },
-  table : {
-    minwidth : 1080
+  menu: {
+    marginLeft: 15,
+    marginRight: 15,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
   },
   progress: {
-    margin: theme.spacing(2)
-  }
-})
+    margin: theme.spacing(2),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  tableHead: {
+    fontSize: '1.0rem'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+});
+
 
 class App extends React.Component {
 
@@ -32,20 +98,27 @@ class App extends React.Component {
     super(props);
     this.state = {
       customers: "",
-      completed: 0
+      completed: 0,
+      searchkeyword: ''
     }
   }
 
   stateRefresh = () => {
     this.setState({
       customers: '',
-      completed: 0
+      completed: 0,
+      searchkeyword: ''
     });
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
     }
   
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
@@ -66,25 +139,65 @@ class App extends React.Component {
   }
 
   render() {
+    const filteredComponents = (data) => {
+      data = data.filter((c) => {
+        return c.name.indexOf(this.state.searchkeyword) > -1
+      })
+      return data.map((c) => {
+        return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+      })
+    }
   const { classes } = this.props;
+  const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "직업", "설정"]
   return (
-    <div>
-      <Paper className={classes.root}>
+    <div className={classes.root}>
+      <AppBar position="static">
+
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            고객 관리 시스템
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="검색하기"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              name = "searchkeyword"
+              value = {this.state.searchkeyword}
+              onChange = {this.handleValueChange}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.menu}>
+      <CustomerAdd stateRefresh = {this.stateRefresh} />
+      </div>
+      <Paper className={classes.paper}>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-              <TableCell>설정</TableCell>
+              {cellList.map(c => {
+                return <TableCell className={classes.tableHead}>{c}</TableCell>
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
-            {this.state.customers ? this.state.customers.map(c => {
-            return( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> )}) : 
+            {this.state.customers ? 
+            filteredComponents(this.state.customers) : 
           <TableRow>
             <TableCell colSpan="6" align="center">
               <CircularProgress className = {classes.progress} variant = "determinate" value = {this.state.completed}/>
@@ -94,7 +207,6 @@ class App extends React.Component {
           </TableBody>
         </Table>
       </Paper>
-      <CustomerAdd stateRefresh = {this.stateRefresh} />
     </div>
   )
 }
--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+})
 
 class CustomerAdd extends React.Component {
 
@@ -11,7 +24,8 @@ class CustomerAdd extends React.Component {
             birthDay: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         }
     };
 
@@ -31,6 +45,24 @@ class CustomerAdd extends React.Component {
         return post(url, formData, config);
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthDay: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
+        })
+    }
+
     handleFromSubmit = (e) => {
         e.preventDefault()
         this.addCustomer()
@@ -44,7 +76,8 @@ class CustomerAdd extends React.Component {
             birthDay: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
     }
 
@@ -63,18 +96,35 @@ class CustomerAdd extends React.Component {
     }
 
     render() {
+        const { classes } = this.props;
         return (
-            <form onSubmit={this.handleFromSubmit}>
-                <h1>고객 추가</h1>
-                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleiFileChange}/> <br/>
-                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValuechange} /> <br/>
-                생년월일: <input type="text" name="birthDay" value={this.state.birthDay} onChange={this.handleValuechange} /> <br/>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValuechange} /> <br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValuechange}/> <br/>
-                <button type="submit">추가하기</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    고객 추가하기
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleiFileChange}/> <br/>
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br />
+                        <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValuechange} /> <br/>
+                        <TextField label="생년월일" type="text" name="birthDay" value={this.state.birthDay} onChange={this.handleValuechange} /> <br/>
+                        <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValuechange} /> <br/>
+                        <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValuechange}/> <br/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFromSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         );
     }
 } 
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
